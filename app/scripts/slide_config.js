8dbc62ee4122f8518c59df3f10f8097e6cd16af7
drei01 <mcreidie@hotmail.com>
2013-04-11 12:20:22 +0100

    update slide config

--- app/scripts/slide_config.js
+++ app/scripts/slide_config.js
@@ -3,12 +3,10 @@ var SLIDE_CONFIG = {
   settings: {
     title: 'Title Goes Here<br>Up To Two Lines',
     subtitle: 'Subtitle Goes Here',
-    //eventTitle: 'Google I/O 2013',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
-    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
     favIcon: 'images/google_developers_logo_tiny.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
@@ -19,12 +17,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title, Google',
+    name: 'Matthew Reid',
+    company: 'Developer, Naked Wines',
     gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    twitter: '@Matthew_Reid',
+    www: 'http://www.matt-reid.co.uk',
+    github: 'http://github.com/drei01'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
